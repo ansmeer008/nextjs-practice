0084019dc549605b5507c418a5354622582954ff
ansmeer008 <ansmeer008@gmail.com>
2023-03-06 22:26:13 +0900

    feat: nested Router 생성

---
+++ pages/movies/all.js
@@ -0,0 +1,5 @@
+//이 파일의 컴포넌트는 "movies/all"로 접속하면 실행된다
+
+export default function All() {
+  return "all";
+}
---
+++ pages/movies/index.js
@@ -0,0 +1,5 @@
+//index.js파일을 만들어주면 "movies"라는 경로로 이동했을 때 이 페이지가 나온다.
+
+export default function All() {
+  return "movies index";
+}
