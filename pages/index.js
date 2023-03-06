f977d10e70dff4ce6268fe07850bcc7edd7ce4c5
ansmeer008 <ansmeer008@gmail.com>
2023-03-06 22:41:30 +0900

    feat: useRouter Hook을 이용해 navigate 하는 방법 적용

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,21 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id) => {
+    router.push(`/movies/${id}`);
+  };
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <Link key={movie.id} href={`/movies/${movie.id}`}>
-          <div className="movie">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-            <h4>{movie.original_title}</h4>
-          </div>
-        </Link>
+        <div key={movie.id} className="movie" onClick={() => onClick(movie.id)}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.id}`}>{movie.original_title}</Link>
+          </h4>
+        </div>
       ))}
       <style jsx>{`
         .container {
